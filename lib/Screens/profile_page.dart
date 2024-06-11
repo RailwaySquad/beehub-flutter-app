1661f0a1dc186237360030bbc2e0fbdd07128b6b
Cat Tuong <88124873+blackcoffee19@users.noreply.github.com>
2024-06-11 08:20:11 +0700

    11/6 Tuong push

--- lib/Screens/profile_page.dart
+++ lib/Screens/profile_page.dart
@@ -20,6 +20,7 @@ class _ProfilePageState extends State<ProfilePage> {
   }
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
     Profile? profile = Provider.of<UserProvider>(context).profile;
     bool isLoading = Provider.of<UserProvider>(context).isLoading;
     if(isLoading || profile==null){
@@ -29,50 +30,77 @@ class _ProfilePageState extends State<ProfilePage> {
     }
     return Scaffold(
       body: Container(
-        child: Column(
+        child: Stack(
+          clipBehavior: Clip.none,
           children: [
-            SizedBox(
+            Container(
+                decoration: const BoxDecoration(
+                  border:  Border(bottom: BorderSide(color: TColors.secondary,width: 2.0))
+                ),
               height: 180,
+              width: size.width,
               child: Container(
                 color:TColors.darkerGrey,
+                child: Image.asset("assets/avatar/jade.png",fit: BoxFit.cover,),
                 ),
             ),
-            // Stack(
-              // alignment: AlignmentDirectional.topStart,
-              // clipBehavior: Clip.none,
-              // children: <Widget> [
-              //   Positioned(
-              //     top: 0, 
-              //     left: 20,
-              //     child: SizedBox(
-              //       width: 80,
-              //       height: 80,
-              //       child: CircleAvatar(
-              //         child: Image.asset("assets/avatar/fuxuan.png",height: 80, width: 80,fit: BoxFit.fill,),
-              //       ),
-              //     ),
-              //   ),
-                // Column(
-                //   children: [
-                //     Expanded(
-                //       child: Column(
-                    
-                //         children: [
-                //           Row(
-                //             children: [
-                //               Text(profile.fullname, style:  Theme.of(context).textTheme.bodyLarge!.copyWith(fontWeight: FontWeight.bold),)
-                //             ],
-                //           ),
-                          
-                //         ],
-                //       ),
-                //     ),
-                //   ],
-                // ),
-                
-          //       ],
-          //   )
-          ],
+            Positioned(
+              top: 120, 
+              left: 20,
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black,width: 2.0),
+                  borderRadius: BorderRadius.circular(45.0)
+                ),
+                width: 90,
+                height: 90,
+                child: CircleAvatar(
+                  backgroundColor:  Colors.white,
+                  child: Image.asset("assets/avatar/fuxuan.png",height: 75, width: 75,fit: BoxFit.fill,),
+                ),
+              ),
+            ),
+            Container(
+              width: size.width,
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  const SizedBox(
+                    height: 180,
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Row(
+                        
+                        children: [
+                          Container(
+                            padding: const EdgeInsets.symmetric(horizontal: 30),
+                            height: 70,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Text(profile.fullname, style: Theme.of(context).textTheme.titleLarge!.copyWith(fontWeight: FontWeight.bold)),
+                                Text("@${profile.username}", style: Theme.of(context).textTheme.labelMedium,)
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                      OutlinedButton(
+                          onPressed: (){},
+                          child: const Text("Account Setting", style: TextStyle(color: TColors.buttonPrimary),),
+                        ),
+                    ],
+                  )
+                ],
+              ),
+            )
+          ]
         ),
       ),
     );
