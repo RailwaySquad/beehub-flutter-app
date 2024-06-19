66867b99ee48e851d3bcfb15dbbe252ffadc5304
Cat Tuong <88124873+blackcoffee19@users.noreply.github.com>
2024-06-19 17:52:00 +0700

    19/6 Tuong add new screen

---
+++ lib/Screens/GroupManager/group_notification.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class GroupNotification extends StatefulWidget {
+  const GroupNotification({super.key});
+
+  @override
+  State<GroupNotification> createState() => _GroupNotificationState();
+}
+
+class _GroupNotificationState extends State<GroupNotification> {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
